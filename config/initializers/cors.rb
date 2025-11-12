6f072e790d96bec3420bc847867210556f9c61a6
Juliana R Fernandes <littlejuh@live.it>
2025-11-12 10:12:48 -0300

    add cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5173'
-    origins 'https://products-react-2la2.onrender.com/'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins 'https://products-react-2la2.onrender.com', 'http://localhost:5173'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Authorization'],
+      credentials: true
   end
 end
